050dce417e99a181139607847b321d725b22cb14
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-09-26 20:20:56 +0300

    index.js

--- index.js
+++ index.js
@@ -7,10 +7,42 @@
  * If you click on button +, the text will be "+"
  * ,... and so on
  */
-function buttonClick(text) {
+
+ //const firstNumber = ("Please enter first number:  ");
+ //const secondNumber = ("Please enter second number:");
+// const operation = ("Please enter an operation (+, -, *, /)");
+
+ let result = 
+    if (isNaN(parseInt(firstNumber)) || isNaN(parseInt(secondNumber))){
+    console.log("not a number")
+  function buttonClick(text) {
   console.log("Clicking", text);
+
+  printOnConsole(text);
+
+  if (operation == '+') {
+    result = parseInt(firstNumber) + parseInt(secondNumber);
+    console.log(firstNumber+ " " + operation+ " "+ secondNumber +" = "+ result);
+}
+
+
+
+//  if (buttonClick === 'clear'){
+  //  indexedDB.html = "";
+  //  onlydecimal= false;
+
+ // }
+
+
+  //function deleteItems() {
+    //localStorage.clear();
+
+ 
+ //onclick(buttonClick('AC'))
   // Write your code here
 }
+
+
 
 /** Supporting functions
  * 1. `printOnConsole(text)`, takes any text, and renders the console part of the web page
